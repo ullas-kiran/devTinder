967fbdfcd7f0df166f893001953c9bc856fc3eca
ullas-kiran <ullas261@gmail.com>
2025-07-13 01:41:32 +0530

    test

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const express=require('express');
 const app=express();
 
-app.use("/",(req,res)=>{
-    res.send("hello from server")
+
+
+app.get("/user",(req,res)=>{
+    res.send({firstName:'ullas',lastName:'kiran'})
+})
+
+app.post("/user",async(req,res)=>{
+    res.send("successfully post")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("successfully deleted")
 })
 
 app.listen(3000,()=>(console.log("app running port 3000")))
